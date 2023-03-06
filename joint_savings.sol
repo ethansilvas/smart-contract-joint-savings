c9eec64f2ddca45dd9e70e397bd7952fa3c2adae
Ethan Silvas <ethansilvas@gmail.com>
2023-03-06 11:58:08 -0800

    deposit, setAccounts, and fallback

--- joint_savings.sol
+++ joint_savings.sol
@@ -29,8 +29,8 @@ contract JointSavings {
     address payable accountOne;
     address payable accountTwo;
     address public lastToWithdraw;
-    uint public lastWithdrawAmount
-    unit public contractBalance;
+    uint public lastWithdrawAmount;
+    uint public contractBalance;
 
     /*
     Define a function named **withdraw** that will accept two arguments.
@@ -47,7 +47,7 @@ contract JointSavings {
         /*
         Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
         */
-        require(balance >= amount, "Insufficient funds");
+        require(contractBalance >= amount, "Insufficient funds");
 
         /*
         Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
@@ -60,32 +60,31 @@ contract JointSavings {
         recipient.transfer(amount);
 
         // Set  `lastWithdrawAmount` equal to `amount`
-        lastWithdrawAmount = amount
+        lastWithdrawAmount = amount;
 
         // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
-        contractBalance = address(this).balance
+        contractBalance = address(this).balance;
     }
 
     // Define a `public payable` function named `deposit`.
     function deposit() public payable {
-
         /*
         Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
         */
-        // YOUR CODE HERE!
+        contractBalance = address(this).balance;
     }
 
     /*
     Define a `public` function named `setAccounts` that receive two `address payable` arguments named `account1` and `account2`.
     */
     function setAccounts(address payable account1, address payable account2) public{
-
         // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
-        // YOUR CODE HERE!
+        accountOne = account1;
+        accountTwo = account2;
     }
 
     /*
     Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
     */
-    // YOUR CODE HERE!
+    function() external payable {}
 }
